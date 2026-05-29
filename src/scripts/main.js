d6ccd101cda22a652f3eb148b43612e2907ef16e
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-05-29 22:27:34 +0100

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const listItemsLinks = document.querySelectorAll('.list-item__link');
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (e) => {
+  listItemsLinks.forEach((link) => {
+    link.href = '#';
+  });
+
+  largeImg.src = e.target.src;
+});
